8246c1b397030855b8817d7bd67820a48f62c0e0
Harish Ravichandiran <60214803+HarishRavichandiran@users.noreply.github.com>
2024-03-13 14:06:44 +0530

    Update CyberSecurity.js

--- src/pages/CyberSecurity.js
+++ src/pages/CyberSecurity.js
@@ -13,7 +13,7 @@ const CyberSecurity = () => {
               <p className="card-text">
                 This is a brief description of the Cyber Security course offered at Guvi.
               </p>
-              <a href="#" className="btn btn-primary">
+              <a className="btn btn-primary">
                 Learn More
               </a>
             </div>
